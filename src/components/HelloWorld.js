db2bc2b21d06a545a70f1c4954b6d1cc1225dba8
nickryanweber <nicholasryanweber@gmail.com>
2019-08-29 16:01:35 -0400

    added some love for Mark

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class HelloWorld extends Component {
   render() {
-    return <>Nick is a swell guy!!!!</>
+    return <>Nick is a swell guy!!!! So is Mark</>
   }
 }
 
