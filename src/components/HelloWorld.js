2e98e51d4efa9bf9cf935c27710a4d5ab9c9a7f7
nickryanweber <nicholasryanweber@gmail.com>
2019-08-29 15:54:53 -0400

    Testing issues with GIT

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class HelloWorld extends Component {
   render() {
-    return <>Nick was here!!!!</>
+    return <>Nick was here!!!! But really he wasn't.</>
   }
 }
 
